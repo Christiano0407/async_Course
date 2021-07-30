b6116f9183cbdde2c6348773366c31c1e1edc535
Christiano0407 <placebo0704@gmail.com>
2021-07-30 15:26:52 -0500

    timeOut

--- landing/js/appLanding.js
+++ landing/js/appLanding.js
@@ -8,6 +8,44 @@ let navLinks = Array.from(document.querySelectorAll(`.nav-link`));
 let contact = document.querySelector(`.contact`); 
 
 
+menuTog.addEventListener(`click`, () => {
+    console.log("MenuToggle"); 
+     // Menu => 
+    menuTog.classList.toggle(`active`); 
+    // Nav => 
+    nav.classList.toggle(`active`); 
+    // Nav Left => 
+    navLeft.classList.toggle(`active`); 
+    // Nav Right => 
+    navRight.classList.toggle(`active`); 
+    // Nav Links => 
+    
+    // contact => 
+
+    // <<<<< ===== Remove Active Class to restart effects ===== >>>>
+    contact.classList.remove(`active`); 
+    navRight.classList.remove(`active`); 
+
+    // >>>>>> Conditionals <<<<<< ========== > 
+    if(menuTog.classList.contains(`active`)) {
+        setTimeout(() => {
+            navRight.classList.add(`active`); 
+        }, 100); 
+
+        for(let i = 0; i < navLinks.length; i ++) {
+            navLinks[i].classList.remove(`active`); 
+
+            setTimeout(() => {
+            navLinks[i].classList.add(`active`); 
+            }, i * 100); 
+        }
+
+        setTimeout(() => {
+            contact.classList.add(`active`); 
+        }, 700); 
+    }
+}); 
+
 
 
 console.groupEnd(); 
