669a90f4e67d6437616e5758279099dbb0b9b4bb
Christiano0407 <placebo0704@gmail.com>
2021-08-09 09:37:45 -0500

    swiper-finished

--- extras/appCarousel.js
+++ extras/appCarousel.js
@@ -1,5 +1,29 @@
 console.group("Carousel One"); 
 
-
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+   /*  slidesPerView: 4, */
+   /*  spaceBetween: 30, */
+   /*  centeredSlides: true, */
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      //clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 
 console.groupEnd(); 
