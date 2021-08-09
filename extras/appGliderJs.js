7ffe5b6437d9512748fb6c6f1a60ea5a2a97b5ad
Christiano0407 <placebo0704@gmail.com>
2021-08-09 11:52:52 -0500

    carousel-A

--- extras/appGliderJs.js
+++ extras/appGliderJs.js
@@ -1,5 +1,14 @@
 console.group("Glider Js"); 
 
-
+new Glider(document.querySelector(`.glider`), {
+    slidesToShow: 4,
+    slidesToScroll: 5,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  });
 
 console.groupEnd(); 
