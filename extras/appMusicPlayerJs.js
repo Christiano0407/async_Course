3e90a659bfe9901e1971a1e01a01fe1207a4beb2
Christiano0407 <placebo0704@gmail.com>
2021-08-09 15:17:20 -0500

    play-1

--- extras/appMusicPlayerJs.js
+++ extras/appMusicPlayerJs.js
@@ -1,5 +1,25 @@
 console.group("MusicPlayer"); 
 
+// Song Data ==== > 
+const songList = [
+    {
+      title: "Audio One",
+      file: "audioOne.mp3", 
+      cover: "1.jpg"
+    }, 
+    {
+      title: "Alegria",
+      file: "/extras/audio/audioblocks-heros-path_HV33f5QSu_WM-SBA-346470570-preview.mp3",
+      cover: "2.jpg",
+    },
+    {
+      title: "Creative World",
+      file:"/extras/audio/audioblocks-in-my-heart_rkd92NBhH_WM-SBA-300416957-preview.mp3",
+      cover: "3.jpg",
+    },
+]; 
+
+// >> Variables <<< Const ================ 
 
 
 console.groupEnd(); 
