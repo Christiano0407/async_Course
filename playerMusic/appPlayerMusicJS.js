b1d8fa153b4e3ac0a9516ba65d9af41519e08197
Christiano0407 <placebo0704@gmail.com>
2021-08-11 12:58:20 -0500

    final-player

--- playerMusic/appPlayerMusicJS.js
+++ playerMusic/appPlayerMusicJS.js
@@ -1,8 +1,25 @@
 console.group("Music Player Neomorfismo"); 
+//) > ================ Variables =================== <
+const audio = document.getElementById(`audio`); 
+const playPause = document.getElementById(`play`); 
 
+//) > ================ Funciones y eventos =================== <
+playPause.addEventListener(`click`, () => {
 
+    if(audio.paused || audio.ended) {
+       playPause.querySelector(`.pause-btn`).classList.toggle(`hide`); 
+       playPause.querySelector(`.play-btn`).classList.toggle(`hide`); 
+       audio.play();
 
+    }else {
+       audio.pause(); 
+       playPause.querySelector(`.pause-btn`).classList.toggle(`hide`); 
+       playPause.querySelector(`.play-btn`).classList.toggle(`hide`); 
+    }
+}); 
 
-
+//) > ================ Ejecutar =================== <
+console.log(audio); 
+console.log(playPause); 
 
 console.groupEnd(); 
