1098f3dc0d236650b3a511e39e17653a628459de
Christiano0407 <placebo0704@gmail.com>
2021-08-10 23:08:03 -0500

    carouselTwoPart1}

--- carouselTwo/appCarouselTwo.js
+++ carouselTwo/appCarouselTwo.js
@@ -1,4 +1,31 @@
 console.group("CarouselTwo"); 
+// ============== Variables ===================================== >
+let images = [...document.querySelectorAll(`.img`)]; 
+console.log(images); 
+
+let slider = document.querySelector(`.slider`); 
+let innerSlider = document.querySelector(`.slider-inner`); 
+let pressed = false; 
+let startX; 
+let X; 
+
+
+// ===================== Eventos and functions ============================ >
+images.forEach((img, idx) => {
+   img.style.backgroundImage = `url(/carouselOne/images/${idx+1}.jpg)`; 
+}); 
+
+slider.addEventListener(`mousedown`, (e) => {
+   pressed = true; 
+   startX = e.offsetX - innerSlider.offsetLeft; 
+   //console.log(innerSlider.offsetLeft); 
+   slider.style.cursor = "grabbing"; 
+}); 
+
+slider.addEventListener(`mouseenter`, () => {
+    slider.style.cursor = "grab"; 
+})
+
 
 
 
