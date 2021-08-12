48ebbc94df66da3a0973e080d1133b350717374c
Christiano0407 <placebo0704@gmail.com>
2021-08-11 21:45:56 -0500

    musics

--- playerMusic/appVarianteJS.js
+++ playerMusic/appVarianteJS.js
@@ -1,8 +1,39 @@
 console.group("Variante Music Player"); 
+// ================= Variables =========================== > 
+const audio = document.getElementById(`audio`); 
+console.log(audio); 
+const title = document.getElementById(`title`); 
+const prev = document.querySelector(`.prev`); 
+const play = document.querySelector(`.play`); 
+const next = document.querySelector(`.next`); 
 
-
-
-
-
+// ============================= Events / Function ================= > 
+const songList = [
+    {
+      path: "audioOne.mp3",
+      songName: "Dreams on Night",
+    }, 
+    {
+        path: "audioblocks-heros-path_HV33f5QSu_WM-SBA-346470570-preview.mp3",
+        songName: "Following The Sun",
+    },
+    {
+        path: "audioblocks-in-my-heart_rkd92NBhH_WM-SBA-300416957-preview.mp3",
+        songName: "I feel love",
+    },
+    {
+        path: "audioblocks-heros-path_HV33f5QSu_WM-SBA-346470570-preview.mp3",
+        songName: "Night and Sun",
+    }, 
+    {
+        path: "audioOne.mp3",
+        songName: "Light of the Night",
+    },
+    {
+        path: "audioblocks-in-my-heart_rkd92NBhH_WM-SBA-300416957-preview.mp3",
+        songName: "Light of the Night",
+    },   
+]
+console.log(songList); 
 
 console.groupEnd(); 
