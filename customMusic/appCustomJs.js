97970a6fb119f7548726ea7fac70557083dd522b
Christiano0407 <placebo0704@gmail.com>
2021-08-13 11:08:59 -0500

    play-allMusic

--- customMusic/appCustomJs.js
+++ customMusic/appCustomJs.js
@@ -1,6 +1,49 @@
 console.group("PLy Three Music"); 
+//  ============================= VARIABLES ========================================= >
+let allMusic = [
+    {
+      name: "Harley Bird - Home", 
+      artist: "Jordan Schor", 
+      img: "/customMusic/img/1.jpg",
+      src: "/customMusic/audio/playFour.mp3",
+    },
+    {
+        name: "Ikson Anywhere - Ikson", 
+        artist: "Audio Library", 
+        img: "/customMusic/img/2.jpg" ,
+        src: "/customMusic/audio/playOne.mp3",
+      },
+      {
+        name: "Lost Sky - Vision NCS", 
+        artist: "NCS Release", 
+        img: "/customMusic/img/3.jpg" ,
+        src: "/customMusic/audio/playTwo.mp3",
+      },
+      {
+        name: "Beauz & Jvna - Crazy", 
+        artist: "Beauz & jvna", 
+        img: "/customMusic/img/4.jpg" ,
+        src: "/customMusic/audio/playThree.mp3",
+      },
+      {
+        name: "Hardwin - Want Me", 
+        artist: "Mike Archangelo", 
+        img: "/customMusic/img/5.jpg" ,
+        src: "/customMusic/audio/playFour.mp3",
+      },
+      {
+        name: "Jim - Sun Goes Down", 
+        artist: "Jim Yosef x Roy", 
+        img: "/customMusic/img/6.jpg" ,
+        src: "/customMusic/audio/playOne.mp3",
+      },
+    
+]; 
+console.log(allMusic); 
 
 
+
+// =========================== EVENTS / FUNCTIONS =================================> 
 
 
 
